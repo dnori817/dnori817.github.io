b1520669039c0559b83b1fe767faa595dacc9099
Nori <dnori817@gmail.com>
2018-04-22 15:49:45 -0400

    More tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 var elem = document.querySelector('.carousel');
 var instance = M.Carousel.init(elem);
+
+
+  var elem2 = document.querySelector('.parallax');
+  var instance = M.Parallax.init(elem2);
+
+
+  var elem3 = document.querySelector('.fixed-action-btn');
+  var instance = M.FloatingActionButton.init(elem3);
